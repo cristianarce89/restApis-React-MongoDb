ef7bf4a32d8c011730adacd424fa2fb2a4f544da
Cristian Arce <cristiancamilo_1989@hotmail.com>
2023-02-05 17:45:55 -0500

    creacion del model pedido y metodos nuevoPedido y mostrarPedido

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const clienteController = require('../controllers/clienteController')
 const productosController = require('../controllers/productosController')
+const pedidosController = require('../controllers/pedidosController')
 
 module.exports = function () {
     // Agregar nuevos clientes via POST
@@ -41,6 +42,16 @@ module.exports = function () {
 
     //Eliminar productos
     router.delete('/productos/:idProducto', productosController.eliminarProducto);
+
+    /************************PEDIDOS****************************************/
+
+    //Agrega nuevos pedidos
+    router.post('/pedidos', pedidosController.nuevoPedido);
+
+    //Mostrar todos los pedidos
+    router.get('/pedidos', pedidosController.mostrarPedidos);
+
+
 
     return router;
 }
